7b03f51e3a301509bfe27c6b11401537bb652eeb
dragonisw <nhoangnam02@gmail.com>
2026-06-03 15:52:25 +0700

    thongtin

--- app/public/wp-content/themes/bookingroom/functions.php
+++ app/public/wp-content/themes/bookingroom/functions.php
@@ -8,8 +8,8 @@
 add_action('wp_enqueue_scripts', 'bookingroom_style');
 function bookingroom_style()
 {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css', array(), time());
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), time());
 
     // Add Google Fonts
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
